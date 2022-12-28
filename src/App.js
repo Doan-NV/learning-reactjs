1bd72ce69f1bd216ac1a95e48376b2103858a0d6
doannv <doannv@meeyland.com>
2022-12-28 16:03:39 +0700

    form handing edit + delete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { useState } from 'react';
 import './App.css';
 import YoutubeList from './components/youtube/YoutubeList';
 import FormComponent from './components/handingForm/FormComponent';
-import { data } from './ytbData';
+import { data, users } from './ytbData';
+import ListViews from './components/cejs79/ListViews';
+import ListViewsMultiForm from './components/cejs79/ListViewsMultiForm';
 function App() {
 
   return (
     <div className="App">
-      <YoutubeList data={data}></YoutubeList>
+      {/* <YoutubeList data={data}></YoutubeList> */}
       {/* <FormComponent></FormComponent> */}
+      {/* <ListViews data={users}></ListViews> */}
+      <ListViewsMultiForm data={users}></ListViewsMultiForm>
     </div>
   );
 }
--- src/ytbData.js
+++ src/ytbData.js
@@ -2,7 +2,7 @@ export const data = [
     {
         id: 1,
         author: "doannv1",
-        title: "somthing like reactjs",
+        title: "<h1>somthing like reactjs</h1>",
         img: "https://loremflickr.com/320/240",
         views: 123
     },
@@ -28,3 +28,14 @@ export const data = [
         views: 123
     }
 ];
+
+export const users = [
+    {
+        id: "231312312312312312312",
+        name: "Doannv",
+    },
+    {
+        id: "312312312312321312312",
+        name: "Nguyen Van Doan"
+    }
+]
